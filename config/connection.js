e3eb8b59b0fd328b56671c882a7c56baf19aca83
Colin Mudie <csmudie1@gmail.com>
2021-03-10 23:32:29 -0500

    removing error messgae

--- config/connection.js
+++ config/connection.js
@@ -11,12 +11,11 @@ const connection = mysql.createConnection({
 
 // Make connection.
 connection.connect((err) => {
-    if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-    }
+    // if (err) {
+    // console.error(`error connecting: ${err.stack}`);
+    // return;
+    // }
     console.log(`connected as id ${connection.threadId}`);
-    console.log("how about now");
 });
 
 // Export connection for our ORM to use.
