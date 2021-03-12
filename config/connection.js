2941bcd1dd7aba3e8509cb4d4a826b66d10c9146
Colin Mudie <csmudie1@gmail.com>
2021-03-11 20:34:05 -0500

    adding JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,25 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+const connection;
 
-const connection = mysql.createConnection({
-    HOST: "us-cdbr-iron-east-02.cleardb.net",
-    USER: "b7e2437887xxxa",
-    PASSWORD: "0200xxx6",
-    DB: "heroku_7643ec736354xxx"
-});
-
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: process.env.MYSQL_PW,
-//     database: 'burger_db',
-// });
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: process.env.MYSQL_PW,
+        database: 'burger_db',
+    });
+}
 
 // Make connection.
 connection.connect((err) => {
-    // if (err) {
-    // console.error(`error connecting: ${err.stack}`);
-    // return;
-    // }
+    if (err) {
+        console.error(`error connecting: ${err.stack}`);
+        return;
+    }
     console.log(`connected as id ${connection.threadId}`);
 });
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,7 @@
 CREATE DATABASE IF NOT EXISTS burger_db;
 USE burger_db;
 
-CREATE TABLE burgers
-(
+CREATE TABLE IF NOT EXISTS `burgers` (
 	`id` INT AUTO_INCREMENT NOT NULL ,
 	`name` varchar(255) NOT NULL,
 	`eaten` BOOLEAN DEFAULT false,
